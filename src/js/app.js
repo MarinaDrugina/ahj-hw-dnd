abfacf5b89e255cdf88dedd44733068e63f8157c
Marina Drugina <druginamarina@gmail.com>
2023-02-07 19:37:27 +0300

    vers 07/02/2023

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import ContentBoard from './ContentBoard.js';
+import TaskColumns from './TaskColumns.js';
+import Engine from './Engine.js';
+import Info from './Info.js';
+
+new ContentBoard();
+new TaskColumns();
+new Engine();
+new Info();
